dd3f210b40f480c3f11902f2824efbbe8dfe0444
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-15 13:00:24 +0100

    ws connection test

--- replApp/init.ts
+++ replApp/init.ts
@@ -1,13 +1,37 @@
 import ajaon from "ajaon"
 import edom from "extended-dom"
+const wsUrl = (document.location.protocol === "https:" ? "wss://" : "ws://") + document.location.host
 
 
-document.body.css("background", "black")
+document.body.css({
+  background: "black",
+  color: "white",
+  padding: 100,
+  fontWeight: "bold",
+  fontFamily: "sans-serif",
+  fontSize: 24
+})
+document.body.txt("Open console")
 
 let { post, get } = ajaon();
 
 
-(async () => {
-  let res = await post("call")
-  console.log(res)
-})()
+const input = document.createElement("input")
+document.body.apd(input as any);
+
+input.on("change", async () => {
+  const ws = new WebSocket(wsUrl + "/" + input.value);
+  let i = 1
+  let pid: any
+  ws.addEventListener("open", () => {
+    console.log("open")
+    pid = setInterval(() => {
+      console.log("Sending hello " + i)
+      ws.send(`Hello ${i++}`)
+    }, 1000)
+  })
+  ws.addEventListener("close", () => {
+    clearInterval(pid)
+    console.log("closing")
+  })
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,34 @@
-import setup from "./setup"
+import { configureExpressApp } from "./../../server/src/setup"
+import expressWs from "express-ws"
+const app = configureExpressApp("/", "public", undefined, (app) => {expressWs(app)}) as ReturnType<typeof configureExpressApp> & { ws: (route: string, fn: (ws: WebSocket & {on: WebSocket["addEventListener"], off: WebSocket["removeEventListener"]}, req: any) => void) => void }
 
 
-
-setup("reactive").then(async ({app, db}) => {
-
+let admins = 1
+app.ws("/admin", (ws) => {
   
-  app.post("/echo", (req, res) => {
-    res.send(req.body)
+  const c = admins
+  ws.on("message", (e) => {
+    console.log(e, "from admin " + c)
   })
+
+  console.log("admin connected", admins++)
 })
+
+let clients = 1
+app.ws("/client", (ws) => {
+  
+  const c = clients
+  ws.on("message", (e) => {
+    console.log(e, "from client " + c)
+  })
+  console.log("client connected", clients++)
+})
+
+
+app.post("/echo", (req, res) => {
+  console.log("post")
+  res.send(req.body)
+
+})
+
+
