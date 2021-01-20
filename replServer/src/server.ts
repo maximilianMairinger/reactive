0645302666f9b79e32dda144510311cdc20b00ba
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-01-20 01:20:01 +0100

    setting up proper repl server with logging

--- replServer/src/server.ts
+++ replServer/src/server.ts
@@ -1,28 +1,31 @@
+import { DataBase } from "josm"
 import liveReloadServer from "./liveReloadServer"
 let app = liveReloadServer("/")
 
 
+const db = new DataBase({
+  abc: {name: "Client1", val: 2}, 
+  abc2: {name: "Client2", val: 4}
+})
+
+let i = 0
 app.ws("/admin", (ws) => {
-  ws.send(JSON.stringify({
-    abc: {name: "Client1", val: 2}, 
-    abc2: {name: "Client2", val: 4}
-  }))
-  
-  let n = 0
-  const pid = setInterval(() => {
-    ws.send(JSON.stringify({abc2: {name: "client " + (n++)}}))
-  }, 5000)
+  const clientNum = i++
+  console.log("opening client " + clientNum)
+  const sub = db((q, diff) => {
+    ws.send(JSON.stringify(diff))
+  }, true, true)
 
-  ws.on("close", () => {
-    clearInterval(pid)
-  })
 
-  console.log("open")
   ws.on("message", (e) => {
-    console.log("msg", e.data)
+    sub.deactivate()
+    db(JSON.parse(e as any))
+    sub.activate(false)
   })
   
 })
+
+
 
 
 
